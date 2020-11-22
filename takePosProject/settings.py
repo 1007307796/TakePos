6038d4c808e400286130004d4990a9e3a1a6487b
1007307796 <1007307796@qq.com>
2020-11-22 20:22:43 +0800

    change to mysql

--- takePosProject/settings.py
+++ takePosProject/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'takePosProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'takepos',
+        'HOST': '',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
